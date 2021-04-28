d855d8d5cfd11dca31d9c8aabf398e9ca4e9a710
LMini562 <71384352+lmini562@users.noreply.github.com>
2021-04-28 19:26:23 +0300

    Create Enemy.cs

---
+++ Enemy.cs
@@ -0,0 +1,50 @@
+sing System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    bool isHit = false;
+    private void OnCollisionEnter2D(Collision2D collision) //в начале столкновения
+    {
+        if (collision.gameObject.tag == "Player" && !isHit) //объект столкновения = player ОБЯЗАТЕЛЬНО
+        {
+            collision.gameObject.GetComponent<Player>().RecountHP(-1); //уменьшаем хп на 1 
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up * 8f , ForceMode2D.Impulse); //типа отталкиваемся от того, что нас коцает
+        }
+    }
+
+    public IEnumerator Death() //карутина на смЭрт
+    {
+        isHit = true; //мы ранимся
+        GetComponent<Animator>().SetBool("dead", true); //делаем смерть правдой
+        GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic; //делаем тело динамическим
+        GetComponent<Collider2D>().enabled = false; //выключаем коллайдер
+        transform.GetChild(1).GetComponent<Collider2D>().enabled = false; //1 располодение ребенка у объекта сверху в низ от 0 и тд
+
+        yield return new WaitForSeconds(2f); //ждем 2 сек
+
+        Destroy(gameObject); //удаляем объект
+    }
+
+    public void startDeath() //метод на смерть
+    {
+        StartCoroutine(Death()); //запуск карутины
+    }
+
+    /*private void OnCollisionStay2D(Collision2D collision) //во время столкновения
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            print("Stay");
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            print("Объекты разошлись :)");
+        }
+    }*/
+}
